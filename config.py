5622a3b6c13537d945bc86b1d1a426ca0b68bb0c
Rahul1l <45888912+Rahul1l@users.noreply.github.com>
2025-10-05 14:26:44 +0530

    config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     # MongoDB Configuration
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://niteshbadgayan:Ganapati%40123@cluster0.lw4vz.mongodb.net/feedback_analysis?retryWrites=true&w=majority&appName=Cluster0')
+    MONGO_URI = os.getenv('MONGO_URI')
     DATABASE_NAME = os.getenv('DATABASE_NAME', 'feedback_analysis')
     COLLECTION_NAME = os.getenv('COLLECTION_NAME', 'feedbacks')
     
@@ -15,8 +15,8 @@ class Config:
     DEBUG = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
     
     # OpenAI Configuration
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', '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')
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
     
     # Admin Configuration
-    ADMIN_EMAIL = os.getenv('ADMIN_EMAIL', 'niteshbadgayan@kpmg.com')
-    ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', 'Ganapati@123')
+    ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
+    ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
